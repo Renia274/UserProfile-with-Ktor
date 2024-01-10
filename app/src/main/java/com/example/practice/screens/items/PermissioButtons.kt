e05773b0eaa92be3d0f5493361fda0583684ac4c
renia274 <erasmia.dioleti@veriah.com>
2024-01-10 13:19:08 +0200

    change the permission  buttons to allow or deny for use the default android launcher for permissions

--- app/src/main/java/com/example/practice/screens/items/PermissioButtons.kt
+++ app/src/main/java/com/example/practice/screens/items/PermissioButtons.kt
@@ -29,3 +29,97 @@ fun MicButton(permissionStateViewModel: PermissionStateViewModel) {
         Text(text = "MIC")
     }
 }
+
+
+
+
+
+//@Composable
+//fun CameraButton(permissionStateViewModel: PermissionStateViewModel) {
+//
+//    val context = LocalContext.current
+//    val launcher = rememberLauncherForActivityResult(
+//        ActivityResultContracts.RequestPermission()
+//    ) { isGranted: Boolean ->
+//        if (isGranted) {
+//            // Permission is granted, perform the necessary actions here
+//            Log.d("Camera", "Camera permission granted")
+//        } else {
+//            // Permission is denied, handle it accordingly
+//            Log.d("Camera", "Camera permission denied")
+//
+//
+//        }
+//    }
+//
+//    Button(
+//        onClick = {
+//            permissionStateViewModel.setCameraPermissionDialogShown(true)
+//            when (PackageManager.PERMISSION_GRANTED) {
+//                ContextCompat.checkSelfPermission(
+//                    context,
+//                    android.Manifest.permission.CAMERA
+//                ) -> {
+//                    // Some works that require permission
+//                    Log.d("Camera", "Camera requires permission")
+//
+//                }
+//
+//                else -> {
+//                    // Asking for permission
+//                    launcher.launch(android.Manifest.permission.CAMERA)
+//                }
+//            }
+//        },
+//        modifier = Modifier.size(width = 100.dp, height = 40.dp)
+//    ) {
+//        Text(text = "CAMERA")
+//    }
+//}
+//
+//
+//
+//@Composable
+//fun MicButton(permissionStateViewModel: PermissionStateViewModel) {
+//
+//    val context = LocalContext.current
+//
+//    val launcher = rememberLauncherForActivityResult(
+//        ActivityResultContracts.RequestPermission()
+//    ) { isGranted: Boolean ->
+//        if (isGranted) {
+//            // Permission is granted, perform the necessary actions here
+//            Log.d("Mic", "Microphone permission granted")
+//
+//        } else {
+//            // Permission is denied, handle it accordingly
+//            Log.d("Mic", "Microphone permission denied")
+//
+//
+//        }
+//    }
+//
+//    Button(
+//        onClick = {
+//            permissionStateViewModel.setMicrophonePermissionDialogShown(true)
+//            when (PackageManager.PERMISSION_GRANTED) {
+//                ContextCompat.checkSelfPermission(
+//                    context,
+//                    android.Manifest.permission.RECORD_AUDIO
+//                ) -> {
+//                    // Some works that require permission
+//                    Log.d("Mic", "Microphone requires permission")
+//                }
+//
+//                else -> {
+//                    // Asking for permission
+//                    launcher.launch(android.Manifest.permission.RECORD_AUDIO)
+//
+//                }
+//            }
+//        },
+//        modifier = Modifier.size(width = 100.dp, height = 40.dp)
+//    ) {
+//        Text(text = "MIC")
+//    }
+//}
