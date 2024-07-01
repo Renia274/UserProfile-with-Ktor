b1ecccac24a6ff111fca863ccb495816df8b60d2
Renia Dioleti <reniadiol3@gmail.com>
2024-07-01 18:27:54 +0300

    Update PinViewModel.kt

--- app/src/main/java/com/example/practice/profiles/viewmodel/pin/PinViewModel.kt
+++ app/src/main/java/com/example/practice/profiles/viewmodel/pin/PinViewModel.kt
@@ -1,48 +1,27 @@
 package com.example.practice.profiles.viewmodel.pin
 
-
-import android.preference.PreferenceManager
 import androidx.lifecycle.ViewModel
-import com.example.practice.MyApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 
 data class PinState(
-    val pin: String?
+    var pin: String = ""
 )
 
+
+
 @HiltViewModel
-class PinViewModel @Inject constructor(
-    private val application: MyApplication
-) : ViewModel() {
+class PinViewModel @Inject constructor() : ViewModel() {
+    private val userPins: MutableMap<String, String> = mutableMapOf()
+    private val pinState: PinState = PinState()
 
-    private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(application)
-
-    fun savePinForProfile(profileName: String, pin: String) {
-        sharedPreferences.edit().putString(getPinKeyForProfile(profileName), pin).apply()
+    fun savePinForProfile(username: String, pin: String) {
+        userPins[username] = pin
+        pinState.pin = pin
     }
 
-    fun getSavedPinForProfile(profileName: String): String? {
-        return sharedPreferences.getString(getPinKeyForProfile(profileName), null)
-    }
-
-    fun matchPinToProfile(pin: String): String? {
-        val profiles = listOf("Bob", "Alice", "Eve")
-        for (profile in profiles) {
-            val savedPin = getSavedPinForProfile(profile)
-            if (pin == savedPin) {
-                return profile
-            }
-        }
-        return null
-    }
-
-    private fun getPinKeyForProfile(profileName: String): String {
-        return "$PIN_KEY.$profileName"
-    }
-
-    companion object {
-        private const val PIN_KEY = "pin_key"
+    fun getPinForProfile(username: String): String? {
+        return userPins[username]
     }
 }
